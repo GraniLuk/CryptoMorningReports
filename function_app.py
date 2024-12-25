287a82d86de1dfecda483533584e7fc3fc859528
Łukasz Mateusz Granica <granica.lukasz@gmail.com>
2024-12-25 15:42:39 +0100

    Average report also works.

--- function_app.py
+++ function_app.py
@@ -1,21 +1,13 @@
 import logging
-import yfinance as yf
-import pandas as pd
-from prettytable import PrettyTable
-from telegram import Bot
 import asyncio
 from datetime import datetime
 import azure.functions as func
-import requests
-from azure.data.tables import TableServiceClient
 import aiohttp
 import os
 from dotenv import load_dotenv
-from collections import namedtuple
-from utils import clean_symbol
 from binance24 import fetch_range_price
 from RSIReport import create_rsi_table
-
+from AverageReport import create_average_table
 load_dotenv()
 
 app = func.FunctionApp()
@@ -35,42 +27,12 @@ def process_bitcoin_checker():
         symbols = ['BTC-USD', 'ETH-USD', 'XRP-USD', 'ATOM-USD', 'DOT-USD', 'HBAR-USD', 'KCS-USD', 'FLOW-USD', 'POL-USD', 'AKT-USD',
                    'NEXO-USD', 'DYM-USD', 'OSMO-USD']
         logging.info('Processing %d symbols...', len(symbols))
-
-        all_values = []
-
-        CryptoData = namedtuple('CryptoData', ['symbol', 'current_price', 'rsi', 'ma50', 'ma200'])
-
-        for symbol in symbols:
-            try:
-                logging.info('Processing symbol: %s', symbol)
-                ticker = yf.Ticker(symbol)
-                df = ticker.history(interval="1d", period="max")
-                logging.info('Retrieved %d data points for %s', len(df), symbol)
-                df['MA50'] = df['Close'].rolling(window=50).mean()
-                today_MA50 = round(df['MA50'].iloc[-1],3)
-                df['MA200'] = df['Close'].rolling(window=200).mean()
-                today_MA200 = round(df['MA200'].iloc[-1],3)
-                # Store the results
-                all_values.append(CryptoData(
-                    symbol=symbol,
-                    ma50=today_MA50,
-                    ma200=today_MA200
-                ))
-            except Exception as e:
-                logging.error('Error processing symbol %s: %s', symbol, str(e))
-
+        
         # Create first table for RSI and prices
         rsi_table = create_rsi_table(symbols)
 
-        average_table = PrettyTable()
-        average_table.field_names = ["Symbol", "Current Price", "MA50", "MA200"]
-        
-        for row in all_values:
-            symbol = clean_symbol(row.symbol)
-            price = row.current_price
-            ma50 = row.ma50
-            ma200 = row.ma200
-            average_table.add_row([symbol, price ,ma50, ma200])
+        # Create second table for 50d and 200d averages
+        average_table = create_average_table(symbols)
 
         # Create second table for 24h ranges
         range_table = fetch_range_price(symbols)
