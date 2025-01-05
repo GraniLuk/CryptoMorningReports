db54528f102281e9f582fe5b8903a2cc0a5eb57a
Łukasz Mateusz Granica <granica.lukasz@gmail.com>
2025-01-05 10:45:38 +0100

    Add also last price.

--- priceRangeReport.py
+++ priceRangeReport.py
@@ -19,7 +19,8 @@ def fetch_kucoin_price(symbol : Symbol, api_key, api_secret, api_passphrase):
         return TickerPrice(
             symbol=symbol.symbol_name,
             low=float(ticker['low']),
-            high=float(ticker['high'])
+            high=float(ticker['high']),
+            last=float(ticker['last'])
         )
     except Exception as e:
         app_logger.error(f"Kucoin error for {symbol}: {str(e)}")
