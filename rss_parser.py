c6a2b64687b5c8b031ffbe1a2efab327291718db
GraniLuk <granica.lukasz@gmail.com>
2025-01-24 11:43:48 +0100

    Add news also.

---
+++ rss_parser.py
@@ -0,0 +1,17 @@
+import feedparser
+
+def fetch_rss_news(feed_url):
+    feed = feedparser.parse(feed_url)
+    latest_news = []
+    for entry in feed.entries[:5]:  # Fetch top 5 news articles
+        latest_news.append({
+            "title": entry.title,
+            "link": entry.link,
+            "published": entry.published,
+            "summary": entry.summary
+        })
+    return latest_news
+
+if __name__ == "__main__":
+    url = "https://decrypt.co/feed"
+    print(fetch_rss_news(url))
