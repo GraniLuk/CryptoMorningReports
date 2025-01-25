d3ec6f1f497b2899fa15f300429692cf2b547489
GraniLuk <granica.lukasz@gmail.com>
2025-01-25 14:36:58 +0100

    Fetch ratio and save to database

--- stepn_ratio_fetch.py
+++ stepn_ratio_fetch.py
@@ -1,40 +1,32 @@
 from azure.monitor.query import LogsQueryClient
 from azure.identity import DefaultAzureCredential
-from datetime import datetime, timedelta
 from telegram_logging_handler import app_logger
 import os
 
 def fetch_gstgmt_ratio_range():
-    # Initialize credentials and Logs Query Client
-    credential = DefaultAzureCredential()
-    client = LogsQueryClient(credential)
+    try:
+        # Initialize credentials and Logs Query Client
+        credential = DefaultAzureCredential()
+        client = LogsQueryClient(credential)
 
-    # Define your Application Insights workspace ID
-    workspace_id = os.getenv('PriceAlerts_APPINSIGHTS_WORKSPACE_ID')
+        # Define your Application Insights workspace ID
+        workspace_id = os.getenv('PriceAlerts_APPINSIGHTS_WORKSPACE_ID')
 
-    # Query to fetch yesterday's min and max
-    query = """
-    customMetrics
-    | where timestamp >= ago(2d) and timestamp < ago(1d)
-    | where name == "crypto_ratio"
-    | extend customMetric_value = iif(itemType == 'customMetric', valueMin, todouble(''))
-    | summarize dailyMin = min(customMetric_value), dailyMax = max(customMetric_value) by bin(timestamp, 1d)
-    """
+        # Query to fetch last 24h min and max
+        query = """
+        customMetrics
+        | where timestamp >= ago(24h)
+        | where name == "crypto_ratio"
+        | summarize min_value = min(valueDouble), max_value = max(valueDouble)
+        """
 
-    # Query time range for the past 2 days
-    end_time = datetime.now()
-    start_time = end_time - timedelta(days=1)
-
-    # Run the query
-    response = client.query_workspace(
-        workspace_id=workspace_id,
-        query=query,
-        timespan=(start_time, end_time),
-    )
-
-    # Process and print results
-    if response.status == 'Success':
-        for table in response.tables:
-            return table.rows[0]
-    else:
-        app_logger.error(f"Query failed: {response.error}")
+        response = client.query_workspace(workspace_id, query)
+        if response and len(response.tables) > 0 and len(response.tables[0].rows) > 0:
+            min_value = response.tables[0].rows[0][0]
+            max_value = response.tables[0].rows[0][1]
+            range_24h = (max_value - min_value) / min_value * 100
+            return min_value, max_value, range_24h
+        return None, None, None
+    except Exception as e:
+        app_logger.error(f"Error fetching ratio range: {str(e)}")
+        return None, None, None
