768617704dd6ec6255f2d6d31a848611e4ae0e63
GraniLuk <granica.lukasz@gmail.com>
2025-02-08 10:03:54 +0100

    Simplify priceRangeReport.

--- technical_analysis/priceRangeReport.py
+++ technical_analysis/priceRangeReport.py
@@ -1,33 +1,19 @@
 from technical_analysis.repositories.priceRangeRepository import save_price_range_results
-from sharedCode.binance import fetch_binance_price
 from prettytable import PrettyTable
-from infra.configuration import get_kucoin_credentials
-from sharedCode.kucoin import fetch_kucoin_price
 from infra.telegram_logging_handler import app_logger
 from typing import List
-from source_repository import SourceID, Symbol
+from source_repository import Symbol
+from sharedCode.priceChecker import fetch_current_price
 
 
 def fetch_range_price(symbols : List[Symbol], conn) -> PrettyTable:
     results = []
-    kucoin_credentials = get_kucoin_credentials()
-    
     for symbol in symbols:
         try:
             # Check if symbol should be fetched from Kucoin
-            if (symbol.source_id == SourceID.KUCOIN):
-                price_data = fetch_kucoin_price(
-                    symbol,
-                    kucoin_credentials['api_key'],
-                    kucoin_credentials['api_secret'],
-                    kucoin_credentials['api_passphrase']
-                )
-                if price_data:
+            price_data = fetch_current_price(symbol)
+            if price_data:
                     results.append(price_data)
-            else:
-                # Regular Binance fetch
-                price_data = fetch_binance_price(symbol)
-                results.append(price_data)
             
             # Save to database if connection is available
             if conn and price_data:
@@ -66,4 +52,15 @@ def fetch_range_price(symbols : List[Symbol], conn) -> PrettyTable:
     return range_table
 
 if __name__ == "__main__":
-    fetch_range_price()
+    from source_repository import Symbol, SourceID
+    from infra.sql_connection import connect_to_sql
+    from dotenv import load_dotenv
+    load_dotenv()
+    conn = None
+    symbols = [
+        Symbol(symbol_id=1, symbol_name="BTC", full_name="Bitcoin", source_id=SourceID.BINANCE),
+        Symbol(symbol_id=2, symbol_name="ETH", full_name="Ethereum", source_id=SourceID.BINANCE)
+    ]
+
+    table = fetch_range_price(symbols, conn)
+    print(table)
