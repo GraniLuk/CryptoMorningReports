4104eed769190f1a76855fd4a525fc3f3d8a9a4d
GraniLuk <granica.lukasz@gmail.com>
2025-04-14 22:00:14 +0200

    Fetch all old candles

--- technical_analysis/daily_candle.py
+++ technical_analysis/daily_candle.py
@@ -29,14 +29,16 @@ def fetch_daily_candles(
 def check_if_all_candles(symbol, conn):
     repo = DailyCandleRepository(conn)
     all_candles = repo.get_all_candles(symbol)
-    oldest_date = all_candles[0].end_date
+    oldest_date = all_candles[0].end_date if all_candles else date(2017, 1, 1)
     if oldest_date:
         print(f"Oldest candle date: {oldest_date}")
     end_date = date.today()
     current_date = oldest_date
     while current_date <= end_date:
         print(f"Fetching data for {current_date}")
-        from_db = next((item for item in all_candles if item.end_date == current_date), None)   
+        from_db = next(
+            (item for item in all_candles if item.end_date == current_date), None
+        )
         if from_db is None:
             fetch_daily_candle(symbol, current_date, conn)
             print("Fetched from API")
@@ -48,11 +50,12 @@ if __name__ == "__main__":
     from dotenv import load_dotenv
 
     from infra.sql_connection import connect_to_sql
-    from source_repository import Symbol, SourceID, fetch_symbols
+    from source_repository import SourceID, Symbol, fetch_symbols
 
     load_dotenv()
     conn = connect_to_sql()
     symbols = fetch_symbols(conn)
+    only_link = [symbol for symbol in symbols if symbol.symbol_name == "LINK"]
     # Define start and end dates for January 2025
-    for symbol in symbols:
+    for symbol in only_link:
         check_if_all_candles(symbol, conn)
