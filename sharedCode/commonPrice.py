751276d949722d1ef09a30e13c346e4d6a2db734
GraniLuk <granica.lukasz@gmail.com>
2025-10-26 16:43:40 +0100

    Implement code changes to enhance functionality and improve performance

--- sharedCode/commonPrice.py
+++ sharedCode/commonPrice.py
@@ -13,26 +13,26 @@ class Candle:
         symbol: str,
         source: int,
         end_date,
-        open_price: float,
         close: float,
         high: float,
         low: float,
         last: float,
         volume: float,
         volume_quote: float,
-        candle_id: int,
+        open: float | None = None,  # noqa: A002
+        id: int | None = None,  # noqa: A002
     ):
         self.symbol = symbol
         self.source = source
         self.end_date = end_date
-        self.open = open_price
+        self.open = open
         self.close = close
         self.high = high
         self.low = low
         self.last = last
         self.volume = volume
         self.volume_quote = volume_quote
-        self.id = candle_id
+        self.id = id
 
 
 # ruff: noqa: N999
